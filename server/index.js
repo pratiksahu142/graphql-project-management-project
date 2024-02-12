8cc233e3b0bd16b1213343a601cb2ef00226b60d
pratiksahu <pratik.sahu1421996@gmail.com>
2024-02-12 18:12:34 -0500

    Project and Client queries

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const colors = require('colors');
+require('dotenv').config();
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+const connectDB = require('./config/db');
+const port = process.env.PORT || 7000;
+
+const app = express();
+
+connectDB();
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development'
+}))
+app.listen(port, console.log(`App running on port ${port}`)); 
