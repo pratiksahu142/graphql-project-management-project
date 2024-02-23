c2517d0173ad90726321fd53fbd7f1155a377c79
pratiksahu <pratik.sahu1421996@gmail.com>
2024-02-22 21:03:19 -0500

    Project detail page, project edit form created

--- client/src/components/ClientInfo.jsx
+++ client/src/components/ClientInfo.jsx
@@ -1,4 +1,4 @@
-import { FaEnvelope, FaPhone, FaIdBadge } from "react-icons/fa";
+import { FaEnvelope, FaPhone, FaIdBadge, FaExclamationCircle } from "react-icons/fa";
 
 
 
@@ -6,18 +6,22 @@ export default function ClientInfo({client}) {
     return (
         <>
             <h5 className="mt-5">Client Information</h5>
-            <ul className="list-group">
-                <li className="list-group-item">
-                    <FaIdBadge className="icon"/>{client.name}
-                </li>
-                <li className="list-group-item">
-                    <FaEnvelope className="icon"/>{client.email}
-                </li>
-                <li className="list-group-item">
-                    <FaPhone className="icon"/>{client.phone}
-                </li>
-
-            </ul>
+            { client ? (
+                <ul className="list-group">
+                    <li className="list-group-item">
+                        <FaIdBadge className="icon"/>{client.name}
+                    </li>
+                    <li className="list-group-item">
+                        <FaEnvelope className="icon"/>{client.email}
+                    </li>
+                    <li className="list-group-item">
+                        <FaPhone className="icon"/>{client.phone}
+                    </li>
+                </ul>
+            ) : (
+                <div><FaExclamationCircle className="icon"/> No client!</div>
+            )}
+            
         </>
     );
 }
